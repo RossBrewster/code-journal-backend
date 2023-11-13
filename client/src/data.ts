354363e4b40d51e0e3af918466b617f2c3b20e5d
Ethan Ostrowski <ethan.ostrowski@gmail.com>
2023-11-13 21:56:42 +0000

    completed delete

--- client/src/data.ts
+++ client/src/data.ts
@@ -7,56 +7,45 @@ export type Entry = UnsavedEntry & {
   entryId: number;
 };
 
-let data = {
-  entries: [] as Entry[],
-  nextEntryId: 1,
-};
-
-window.addEventListener('beforeunload', function () {
-  const dataJSON = JSON.stringify(data);
-  localStorage.setItem('code-journal-data', dataJSON);
-});
-
-const localData = localStorage.getItem('code-journal-data');
-if (localData) {
-  data = JSON.parse(localData);
-}
-
 export async function readEntries(): Promise<Entry[]> {
-    const req = await fetch('/api/entries');
-    const parsedResponse = await req.json();
-    return parsedResponse
+  const req = await fetch('/api/entries');
+  const parsedResponse = await req.json();
+  return parsedResponse;
 }
 
-export async function addEntry(entry: UnsavedEntry): Promise<Entry>{
+export async function addEntry(entry: UnsavedEntry): Promise<Entry> {
   const req = await fetch('/api/entries', {
-    method: "POST",
+    method: 'POST',
     headers: {
-      'content-type': 'application/json'
+      'content-type': 'application/json',
     },
-    body: JSON.stringify(entry)
-  })
-  const responseEntry = await req.json()
-  console.log("response", responseEntry)
+    body: JSON.stringify(entry),
+  });
+  const responseEntry = await req.json();
+  console.log('response', responseEntry);
   return responseEntry;
 }
 
-export async function updateEntry(entry: Entry): Entry {
-  console.log(entry)
+export async function updateEntry(entry: Entry): Promise<Entry> {
+  console.log(entry);
   const req = await fetch(`/api/entries/${entry.entryId}`, {
-    method: "PUT",
+    method: 'PUT',
     headers: {
-      'content-type': 'application/json'
+      'content-type': 'application/json',
     },
-    body: JSON.stringify(entry)
-  })
-  const response = await req.json()
+    body: JSON.stringify(entry),
+  });
+  const response = await req.json();
   return response;
 }
 
-export function removeEntry(entryId: number): void {
-  const updatedArray = data.entries.filter(
-    (entry) => entry.entryId !== entryId
-  );
-  data.entries = updatedArray;
+export async function removeEntry(entryId: number): Promise<void> {
+  const req = await fetch(`/api/entries/${entryId}`, {
+    method: 'DELETE',
+    headers: {
+      'content-type': 'application/json',
+    },
+  });
+  const parsedResponse = await req.json();
+  return parsedResponse;
 }
